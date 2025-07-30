17980a6e14b2af68e19d66694734c1e43f9beaab
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2025-07-30 21:28:26 +0000

    Add nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,38 +1,75 @@
 #include <stdio.h>
 
+void moverTorre(int casas) {
+  if(casas > 0) {
+    printf("Torre: 1 casa para a direita\n");
+    moverTorre(casas - 1);
+  }
+}
+
+void moverBispo(int casas) {
+  if(casas > 0) {
+    // Loops aninhados
+    int movimento, passo;
+    for(movimento = 1; movimento <= 1; movimento++) {
+      for(passo = 1; passo <= 10; passo++) {
+        if(passo == 1) {
+          printf("Bispo: 1 casa cima e à direita\n");
+          break;
+        }
+        continue;
+      }
+    }
+    moverBispo(casas - 1); // Chamada recursiva
+  }
+}
+
+void moverRainha(int casas) {
+  if(casas > 0) {
+    printf("Rainha: 1 casa para a esquerda\n");
+    moverRainha(casas - 1);
+  }
+}
+
 int main() {
   //Torre = linha reta = 5 casas para direita
   printf("Movimento da Torre\n");
-  for(int t = 1; t <= 5; t++) {
-    printf("Torre: 1 casa para a direita\n");
-  }
+  moverTorre(5);
 
   //Bispo = diagonal = 5 casas para cima e à direita
   printf("\nMovimento do Bispo\n");
-  int b = 1;
-  while(b <= 5) {
-    printf("Bispo: 1 casa cima e à direita\n");
-    b++;
-  }
+  moverBispo(5);
 
   //Rainha = todas direções = 8 casa para esquerda
   printf("\nMovimento da Rainha\n");
-  int r = 1;
-  do {
-    printf("Rainha: 1 casa para a esquerda\n");
-    r++;
-  } while (r <= 8);
+  moverRainha(8);
 
-  //Cavalo move em "L" = 2 casas para baixo e 1 para a esquerda
+  //Cavalo move em "L" = 2 casas para cima e 1 para a direita
   printf("\nMovimento do Cavalo\n");
-  int y = 1, x;
+  int movimentos = 1, etapa, passo;
   
-  for(x = 1; x <= 1; x++) {
-    while(y <= 2) {
-      printf("Cavalo: 1 casa para baixo\n");
-      y++;
+  for(movimentos = 1; movimentos <= 1; movimentos++) { //Executa 1 vez controlando quantos movimentos em "L" o cavalo fará
+    
+    for(etapa = 1; etapa <= 2; etapa++) { // Loop externo: executa 2 casas para cima
+      for(passo = 1; passo <= 10; passo++) { //Loop interno: dá um passo pra cima e break
+        if(passo == 1) {
+          printf("Cavalo: 1 casa para cima\n");
+          break;
+        }
+        continue;
+      }
     }
-    printf("Cavalo: 1 casa para a esquerda\n");
+    
+    
+    for(etapa = 1; etapa <= 1; etapa++) {// Loop externo: executa 1 casa para a direita
+      for(passo = 1; passo <= 10; passo++) { //Loop interno: dá um passo para direita e break
+        if(passo == 1) {
+          printf("Cavalo: 1 casa para a direita\n");
+          break;
+        }
+        continue;
+      }
+    }
   }
   return 0;
 }
